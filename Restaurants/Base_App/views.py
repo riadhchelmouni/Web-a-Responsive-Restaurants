a7eebff7c5e5b44aacdbd8f2a4e6432f909464f7
riadhchelmouni <chelmouni.yazidriadh@gmail.com>
2024-12-19 22:34:32 +0100

    first commit

---
+++ Restaurants/Base_App/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def HomeView(request):
+    pass
+
+def AboutView(request):
+    pass
+
+def MenuView(request):
+    pass 
+
+def BookTableView(request):
+    pass
+
